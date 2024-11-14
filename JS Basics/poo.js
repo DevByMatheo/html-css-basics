917cd23a680c8ccb0067291d0a5aea7e13b461b0
DevByMatheo <mathivanov.prog@icloud.com>
2024-11-14 22:17:25 +0100

    Update poo.js

--- JS Basics/poo.js
+++ JS Basics/poo.js
@@ -1,21 +1,60 @@
+// ES6
+
+/* Classe -> Pas d attribut prv
+          -> Pas d'attribut statique
+*/ 
+
 class MusicPlayer {             // let MP = class {}
-    constructor(format) {
-        this.format = format;
+    constructor(filetype) {
+        this.filetype = filetype;
+    }
+/*
+    getFiletype() {
+        return this.filetype;
     }
 
-    getFormat() {
-        return this.format;
+    setFiletype(new_filetype) {
+        return this.filetype = new_filetype;
     }
-
-    setFormat(new_format) {
-        return this.format = new_format;
-    }
-
+*/
     play() {
         console.log("Lecture en cours...");
     }
-}
+
+    static hello() {
+        return 'hello world !'
+    }
+};
+
+/*
+// Version Prototype
+function fMusicPlayer(filetype){
+    this.filetype = filetype;
+};
+// ou 
+function f2MusicPlayer(filetype){
+    var filetype = filetype;
+    this.filetype = function(){return filetype;}
+};
+*/
+
+
+/*console.log(player.getFiletype());
+console.log(player.setFiletype("OGG"));*/
 
 let player = new MusicPlayer("MP3");
-console.log(player.getFormat());
-console.log(player.setFormat("OGG"));
+document.write(player.filetype + "<br>"); // player.filetype() pour f2
+player.filetype = "OGG";
+document.write(player.filetype + "<br>");
+
+
+//fMusicPlayer.hello = function(){ return "Hello World !";};
+//document.write(fMusicPlayer.hello());
+
+document.write(MusicPlayer.hello()+ "<br>");
+
+MusicPlayer.prototype.filename = null;
+player.filename = "C\\Users\\:";
+document.write(player.filename);
+
+delete player.filename;
